53fbd47ca2dbc0f041b67b62f0eeed360a01c9d8
Sudha-SKY <96175627+Sudha-SKY@users.noreply.github.com>
2022-03-11 14:58:58 +0530

    Add files via upload

---
+++ src/store/authSlice.js
@@ -0,0 +1,18 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+const initialAuthState = { isAuthenticated: false };
+
+const authSlice = createSlice({
+  name: "Authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export default authSlice.reducer;
+export const authActions = authSlice.actions;
